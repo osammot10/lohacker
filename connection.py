670f93d24ab04216715b891fe36335b5d9ea7a4b
osammot10 <tommasopellizzon2000@gmail.com>
2021-07-30 18:22:59 +0200

    aggiunto tasto condivisione nella visualizzazione dei propri form

--- connection.py
+++ connection.py
@@ -14,7 +14,7 @@ Base.metadata.schema = 'form'
 # postgresql(nome del server, non cambiare)://username:password@hostname/db_name
 # username e password sono le credenziali con le quali si accede al db
 # hostname è l'hostname o l'ip di dove si trova il server
-engine = create_engine('postgresql://postgres:postgres@localhost/postgres')
+engine = create_engine('postgresql://postgres:postgres@localhost/lohacker')
 metadata = MetaData()
 
 Session = sessionmaker(bind=engine)       # creazione della factory
@@ -140,7 +140,6 @@ class RadioAnswer(Base):
 def load_user ( user_id ):
     rs = session.query(Utenti).filter(Utenti.id == user_id)
     user = rs.first()
-
     return User(user.id, user.email, user.password, user.first_name, user.surname)
 
 def get_user_by_email(email):
